eb73feca862c804ac5262a5d0c2187addd1d0d2a
Profytimmy <juliousmide@gmail.com>
2022-07-28 15:30:08 +0000

    new templates for profile

--- metadata/urls.py
+++ metadata/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .models import ProfilePage
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("signup", views.signup, name="signup"),
-    path("profile", views.profile, name="profile")
+    path("old_profile", views.profile, name="show_profile"),
+    path("profile/<int:pk>", ProfilePage.as_view, name="profile")
 ]
