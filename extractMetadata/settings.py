456914e1f42086fae7a9fb204021a374c29bf713
Profytimmy <juliousmide@gmail.com>
2022-07-28 14:07:22 +0000

    profile template

--- extractMetadata/settings.py
+++ extractMetadata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-reyg=1j=w_ggw_t-_6d@10ia640ui9%(d-=xim5v@hwotv9q*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  [ '127.0.0.1']
 
 
 # Application definition
