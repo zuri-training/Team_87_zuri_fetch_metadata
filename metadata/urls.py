f2eb7991ad1bdabfe8af9fdf0f439135d2f05e93
Popsicool <akinolasamson1234@gmail.com>
2022-07-28 21:55:45 +0100

    add contact

--- metadata/urls.py
+++ metadata/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-app_name= 'metadata'
+app_name = 'metadata'
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("signup", views.signup, name="signup"),
     path("profile/<int:pk>", views.profile.as_view(), name="profile"),
-    
+    path("contact", views.contact, name="contact"),
+
 ]
