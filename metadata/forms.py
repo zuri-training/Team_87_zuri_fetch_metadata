ab55c85b2dbabde35f4af20a076a2f0fd4cde25e
godwin opara <godwinopara62@yahoo.com>
2022-08-03 03:05:47 +0100

    updated form

--- metadata/forms.py
+++ metadata/forms.py
@@ -1,11 +1,4 @@
 from django import forms
-<<<<<<< HEAD
-
-
-class FileUpload(forms.Form):
-    name = forms.CharField(max_length=80)
-    upload_file = forms.FileField()
-=======
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from .models import Profile
@@ -14,9 +7,9 @@ from .models import Profile
 class FileUpload(forms.Form):
     upload_file = forms.FileField()
 
+
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
-        exclude= ['user']
->>>>>>> refs/remotes/origin/main
+        exclude = ['user']
