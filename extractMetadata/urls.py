8a39b0a56ea92035686630d67478a4be4e90a80e
godwin opara <godwinopara62@yahoo.com>
2022-08-06 03:28:12 +0100

    updated with teams contributions

--- extractMetadata/urls.py
+++ extractMetadata/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('metadata.urls')),
     path('', include('django.contrib.auth.urls')),
+    path(r'^download/(?P<path>.*)$', serve,
+         {"document_root": settings.MEDIA_ROOT})
 ]
 
 if settings.DEBUG:
